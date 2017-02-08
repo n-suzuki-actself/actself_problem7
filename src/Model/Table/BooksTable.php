b8ada6bc7edc1434e833fa6aa63e9a856cd8be11
unknown <>
2017-02-08 22:10:36 +0900

    ユーザ同録実装

--- src/Model/Table/BooksTable.php
+++ src/Model/Table/BooksTable.php
@@ -66,37 +66,25 @@ class BooksTable extends Table{
        
     }
     
-    public function averageScore($book_id){
-        $conn = ConnectionManager::get('default');
-        $sql = 'SELECT * FROM reviews WHERE book_id =' . $book_id;
-        $stmt = $conn->query($sql);
-	$rows = $stmt->fetchAll('assoc');
-        echo $book_id;
-        
-        $star_count = $rows['star_count'];
-        
-        //$data = $rows['star_count'];
-        foreach($star_count as $star_count):
-            
-        echo $star_count['star_count'];
-        endforeach;
-        
-        
-        
-        $age = array(40, 39, 35, 33, 32, 31); // 年齢を代入し、配列を初期化。（代入の順番は入れ替えてもOK。）
-
-	$total = array_sum($star_count); // 配列$ageの合計値を求めて、変数$totalに代入。
-
-	$head_count = count($star_count); // 要素数を数えて、変数$head_countに代入。
-
-	$average = round($total / $head_count); // 平均値を求めて、四捨五入する。
-
-	//$top = max($age); // いちばん大きな数を代入。
-        echo $average;
-	//print "<p>"."平均年齢は".$average."歳です。"."</p>\n";
-
-	//print "<p>"."一番上は".$top."歳です。"."</p>";
-        
-    }
+//    public function averageScore($book_id){
+//        $conn = ConnectionManager::get('default');
+//        $sql = 'SELECT * FROM reviews WHERE book_id =' . $book_id;
+//        $stmt = $conn->query($sql);
+//	$rows = $stmt->fetchAll('assoc');
+//        echo $book_id;
+//        
+//        $star_count = $rows['star_count'];
+//        
+//        //$data = $rows['star_count'];
+//        foreach($star_count as $star_count):
+//            
+//        echo $star_count['star_count'];
+//        endforeach;
+//        
+//        
+//        
+//        
+//        
+//    }
     
 }
